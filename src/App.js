df4108948b66d75ba983b6b680c8611f73e9b7fb
“Evgeny <ahotickkk@mail.ru>
2022-03-30 21:37:30 +0300

    learning base react jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+let text = '111';
+
 function App() {
+
+  let text2 = '222';
+
+  const style = {
+   fontSize: '24px',
+   fontStyle : 'italic',
+   color: 'red'
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="container">
+    <img src="/images/test.webp" alt='Картинка'/>
+    <ul>
+      <li style={style}>Hello</li>
+      <li>{text + text2}</li>
+    </ul>
+    <h1>app_1</h1>
+  </div>  
   );
 }
+
 
 export default App;
