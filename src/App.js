d0fb75de1f169aee7e87e4c1fa6c92bdc32f7b04
“Evgeny <ahotickkk@mail.ru>
2022-04-02 23:01:00 +0300

    learn State react -> functional component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Comp1 from './Comp1';
 import { useState } from 'react'
+import Comp2 from './Comp2';
 
 function App() {
 
   return (
     <>
-      <Comp1 />
+      <Comp2/>
     </>
   );
 
